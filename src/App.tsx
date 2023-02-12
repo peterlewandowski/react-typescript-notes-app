ad34103e2c95b7e31d660fbd3097541e4ae7b034
Peter Lewandowski <mapiotr2010@gmail.com>
2023-02-12 14:47:54 -0500

    Delete note button logic

--- src/App.tsx
+++ src/App.tsx
@@ -74,6 +74,12 @@ function App() {
     });
   }
 
+  function onDeleteNote(id: string) {
+    setNotes(prevNotes => {
+      return prevNotes.filter(note => note.id !== id)
+    })
+  }
+
   function addTag(tag: Tag) {
     setTags((prev) => [...prev, tag]);
   }
@@ -96,7 +102,7 @@ function App() {
           }
         />
         <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
-          <Route index element={<Note />} />
+          <Route index element={<Note onDelete={onDeleteNote} />} />
           <Route
             path="edit"
             element={
