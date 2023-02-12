53b96949eaef37ada8df7a6cb6042fb88e80c7b7
Peter Lewandowski <mapiotr2010@gmail.com>
2023-02-12 11:16:32 -0500

    NoteList and NoteCard components

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import NewNote from "./NewNote";
 import { useLocalStorage } from "./useLocalStorage";
 
 import { v4 as uuidV4 } from "uuid";
+import NoteList from "./NoteList";
 
 export type Note = {
   id: string;
@@ -57,7 +58,7 @@ function App() {
   return (
     <Container className="my-4">
       <Routes>
-        <Route path="/" element={<h1>Hi</h1>} />
+        <Route path="/" element={<NoteList notes={notesWithTags} availableTags={tags} />} />
         <Route path="/new" element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags} />} />
         <Route path="/:id">
           <Route index element={<h1>Show</h1>} />
