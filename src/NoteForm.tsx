199038f703878fbf18f3a41d83cc0a90a708b3ef
Peter Lewandowski <mapiotr2010@gmail.com>
2023-02-11 23:22:39 -0500

    added navigate to previous page

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
 import { NoteData, Tag } from "./App";
 import { v4 as uuidV4 } from "uuid";
@@ -16,6 +16,7 @@ function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const navigate = useNavigate()
 
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
@@ -23,8 +24,10 @@ function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
     onSubmit({
       title: titleRef.current!.value,
       markdown: markdownRef.current!.value,
-      tags: [],
+      tags: selectedTags,
     });
+
+    navigate("..")
   }
 
   return (
